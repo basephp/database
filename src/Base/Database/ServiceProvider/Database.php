8c9ede38962fca7ee93a260b22de276e37c6afa0
Timothy Marois <timothymarois@gmail.com>
2018-05-05 22:39:59 -0400

    Added port to database config.

--- src/Base/Database/ServiceProvider/Database.php
+++ src/Base/Database/ServiceProvider/Database.php
@@ -11,8 +11,9 @@ class Database extends ServiceProvider
         $db = DatabaseManager::getInstance();
 
         $db->addConnection('default',[
-            'driver'   => config('db.driver'),
-            'hostname' => config('db.host'),
+            'driver'   => config('db.driver','MySQLi'),
+            'hostname' => config('db.host','127.0.0.1'),
+            'port'     => config('db.port',3306),
             'database' => config('db.name'),
             'username' => config('db.user'),
             'password' => config('db.pass')
